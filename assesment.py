e4fcc92e0fc3761175a1f115cc83c1b120648f4e
Dapo Banjo-Obaleye <user@users-MacBook-Pro.local>
2023-07-25 16:01:25 +0100

    update

---
+++ assesment.py
@@ -0,0 +1,17 @@
+sentences = ("hi my name is dapo." , "the Denver Nuggets are Nba champions." , "Adolf hitler is a Nazi.")
+
+def number_of_sentences():
+   x = max(sentences)
+   print(x)
+number_of_sentences()
+
+random =( 1 , 2 ,"dapo" , "mum" , 3)
+new_list =[]
+def random_new():
+  for i in random:
+     if i is str(i):
+        new_list.append(i)
+        
+random_new()
+print(new_list)
+
